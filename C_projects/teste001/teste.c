509a4e8a9ace25cca94b4923b254395c882dbcab
Leonardo Cremasco <leoboscariol@gmail.com>
2023-04-05 22:32:45 -0300

    pasta de C language

---
+++ C_projects/teste001/teste.c
@@ -0,0 +1,17 @@
+/**************
+VOTO DO ELEITOR
+***************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main ()
+{
+    int resultado;
+    resultado = multiplicacao (6,7);
+    printf ("6 vezes 7 eh %d", resultado);
+    return 0;
+}
