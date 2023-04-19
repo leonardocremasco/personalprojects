64e78aa1bb07f8895be490f1a2a283fe8cc2935e
Leonardo Cremasco <leoboscariol@gmail.com>
2023-04-19 20:25:53 -0300

    initial commit | 19-04-2023

---
+++ C_projects/teste001/aula19-04.c
@@ -0,0 +1,12 @@
+// Aula duia 19-04-2023 //
+
+#include <stdio.h>
+#include <limits.h>
+
+int main ()
+{
+    int resultado;
+    resultado = 6 + 7;
+    printf("&d", resultado);
+    return 0;
+}
--- C_projects/teste001/teste.c
+++ C_projects/teste001/teste.c
@@ -3,17 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int multiplicacao (int p1, int p2)
+int main()
 {
-    int produto;
-    produto = p1 * p2;
-    return produto;
-}
-
-int main ()
-{
-    int resultado;
-    resultado = multiplicacao (6,7);
-    printf ("6 vezes 7 eh %d", resultado);
+    printf("Ola, mundo");
     return 0;
 }
