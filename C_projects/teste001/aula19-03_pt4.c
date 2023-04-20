6e3bb42013be2e8854174d60f899bd54a239d0eb
Leonardo Cremasco <leoboscariol@gmail.com>
2023-04-19 22:14:07 -0300

    final commit | 19-04

---
+++ C_projects/teste001/aula19-03_pt4.c
@@ -0,0 +1,10 @@
+// Testando variáveis do tipo float
+
+#include <stdio.h>
+
+int main ()
+{
+    float lf_peso = 99.75;
+    printf("O meu peso eh: %.2f", lf_peso);
+    return 0;
+}
---
+++ C_projects/teste001/aula19-04_pt3.c
@@ -0,0 +1,20 @@
+// Testando uma variável do tipo inteiro
+
+#include <stdio.h>
+
+
+int main ()
+{
+    int li_resultado;
+    li_resultado = 10;
+
+    printf("O valor da variavel eh: %i mais isso \n", li_resultado);
+    printf("\n \n \n");
+
+    li_resultado = li_resultado+ 5;
+
+    printf("O novo valor da variavel eh: %i", li_resultado);
+    printf("\n \n \n");
+
+    return 0;
+}
