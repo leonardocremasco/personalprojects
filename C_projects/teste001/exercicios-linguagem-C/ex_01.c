15943a2fdc26f68b79cd91a1b37e480c15bd158b
Leonardo Cremasco <leoboscariol@gmail.com>
2023-04-25 10:50:54 -0300

    Update ex_01.c

--- C_projects/teste001/exercicios-linguagem-C/ex_01.c
+++ C_projects/teste001/exercicios-linguagem-C/ex_01.c
@@ -1,4 +1,17 @@
-// Exercício 1
+// Exercício 1 -> Faça um programa que leia um número inteiro e retorne seu antecessor e seu sucessor
 
 #include <stdio.h>
 #include <limits.h>
+
+int main ()
+{
+    int li_numero;
+    int li_antecessor;
+    int li_sucessor;
+
+    printf("Informe um numero inteiro: ");
+    scanf("%d%*c", &li_numero);
+    printf("\n O numero digitado foi: %d", li_numero);
+
+    // calculando o número digitado
+}
