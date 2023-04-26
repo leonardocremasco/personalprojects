d60ef8a265121d793174d3ec1392a698d077bfcf
Leonardo Cremasco <leoboscariol@gmail.com>
2023-04-26 10:05:21 -0300

    C language list | finished.

--- C_projects/teste001/operadores_em_C/ex_operador_igual.c
+++ C_projects/teste001/operadores_em_C/ex_operador_igual.c
@@ -13,8 +13,21 @@ int igualdade (int p1, int p2)
 int main ()
 {
     int primeiro_valor;
+    int segundo_valor;
 
     printf("Digite um valor inteiro para (x): ");
     scanf("%d%*c", &primeiro_valor);
+
+    printf("Digite um valor inteiro para (y): ");
+    scanf("%d%*c", &segundo_valor);
+
+    if (primeiro_valor == segundo_valor) {
+        printf("Os valores de (%d) e (%d) sao iguais.", primeiro_valor, segundo_valor);
+    } else {
+        printf("Os valores de (%d) e (%d) sao diferentes", primeiro_valor, segundo_valor);
+    }
+    
+    return 0;
+
 }
 
