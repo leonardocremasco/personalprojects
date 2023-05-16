aa4ba5bf585c1a81e9b46b45a8f12a1a266bdb33
Leonardo Cremasco <leoboscariol@gmail.com>
2023-05-15 21:40:21 -0300

    update 01

---
+++ web systems paper - 22_05-2023/exercicio10/script.js
@@ -0,0 +1,20 @@
+// Fazendo tudo funcionar por JS
+
+function validateForm(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Definindo constantes de usuário e senha
+    const USERNAME = "usuario";
+    const PASSWORD = "senha";
+
+    // Obtendo os valores digitados pelo usuário
+    const enteredUsername = document.getElementById("username").value;
+    const enteredPassword = document.getElementById("password").value;
+
+    // Verificando se o nome de usuário e senha estão corretos
+    if (enteredUsername === USERNAME && enteredPassword === PASSWORD) {
+      document.getElementById("loginMessage").textContent = "Acesso permitido!";
+    } else {
+      document.getElementById("loginMessage").textContent = "Acesso negado!";
+    }
+}
