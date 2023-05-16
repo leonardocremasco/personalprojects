19c0d1255325acdd46bc3100dde9ad3ff9bb0529
Leonardo Cremasco <leoboscariol@gmail.com>
2023-05-15 22:06:37 -0300

    sei la, tentei

--- web systems paper - 22_05-2023/exercicio01/script.js
+++ web systems paper - 22_05-2023/exercicio01/script.js
@@ -1,7 +1,4 @@
 //  dando vida ao código
 
-let d1 = window.document.querySelector('input#date1')
-let d2 = window.document.querySelector('input#date2')
 
-const diferenca = Math.abs(date1.getTime() - date2.getTime())
 
