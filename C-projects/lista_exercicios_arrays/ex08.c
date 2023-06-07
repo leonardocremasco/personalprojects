cf6e342455f331a1eea33b10903c2a575b65e022
Leonardo Cremasco <leoboscariol@gmail.com>
2023-06-07 10:33:11 -0300

    C array list, finished.

--- C-projects/lista_exercicios_arrays/ex08.c
+++ C-projects/lista_exercicios_arrays/ex08.c
@@ -2,6 +2,26 @@
 
 #include <stdio.h>
 
-int main (){
+int main() {
     
+    float notas[5];
+    float soma = 0.0;
+    int i;
+
+    printf("Digite as notas de 5 estudantes: ");
+    for (i = 0; i < 5; i++) {
+        scanf("%f", &notas[i]);
+        soma += notas[i];
+    }
+
+    float media = soma / 5.0;
+
+    printf("Notas acima da media:\n");
+    for (i = 0; i < 5; i++) {
+        if (notas[i] > media) {
+            printf("nota: %.2f\n", notas[i]);
+        }
+    }
+
+    return 0;
 }
--- C-projects/lista_exercicios_arrays/ex09.c
+++ C-projects/lista_exercicios_arrays/ex09.c
@@ -2,23 +2,34 @@
 
 #include <stdio.h>
 #include <ctype.h>
+
 #define NOTAS 100
 
 int main () {
-
+    
     int i;
-    float media = 0;
+    float media_1;
+    float media_2 = 0;
 
-    printf("Digite as notas dos estudantes:\n");
+    printf("Digite as notas dos estudantes\n");
     for (i = 0; i < NOTAS; i++){
         printf("Nota do estudante %d:", i);
-        scanf("%f", &NOTAS[i]);
+        scanf("%f", &media_1);
     }
 
-    for (i = 0; i < NOTAS; i++) {
-        media = media + NOTAS[i];
-        media = media/100;
+    for (i = 0; i < NOTAS; i++){
+        media_2 = media_2 + media_1;
+        media_2 = media_2 / NOTAS;
     }
+
+    for (i = 0; i < NOTAS; i++){
+        if (media_1 > media_2)
+        {
+            printf("Notas: %f\n", media_1);
+        }
         
+    }
+    
+    return 0;
     
 }
