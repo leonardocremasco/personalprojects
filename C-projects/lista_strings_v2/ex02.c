cdca09e57ab138d9208efd6e6855f611493b0a0e
Leonardo Cremasco <leoboscariol@gmail.com>
2023-08-18 20:53:00 -0300

    coding and fixing errors

--- C-projects/lista_strings_v2/ex02.c
+++ C-projects/lista_strings_v2/ex02.c
@@ -1,1 +1,53 @@
 // Ex02) Leia uma frase de até 255 caracteres e mostre a qtde de vogais, a qtde de consoantes, a qtde de espaços e a qtde de palavras na frase
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char frase[256];
+    int vogais = 0, consoantes = 0, espacos = 0, palavras = 0;
+    
+    printf("Digite uma frase (até 255 caracteres): ");
+    fgets(frase, sizeof(frase), stdin);
+    
+    // Removendo o caractere de nova linha gerado pelo fgets
+    frase[strcspn(frase, "\n")] = '\0';
+    
+    int i = 0;
+    while (frase[i] != '\0') {
+        char c = tolower(frase[i]);
+        
+        if (c >= 'a' && c <= 'z') {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vogais++;
+            } else {
+                consoantes++;
+            }
+        } else if (c == ' ') {
+            espacos++;
+        }
+        
+        i++;
+    }
+    
+    // contando qtd. de palavras
+    i = 0;
+    while (frase[i] != '\0') {
+        if (isalpha(frase[i])) {
+            while (isalpha(frase[i])) {
+                i++;
+            }
+            palavras++;
+        } else {
+            i++;
+        }
+    }
+    
+    printf("Quantidade de vogais: %d\n", vogais);
+    printf("Quantidade de consoantes: %d\n", consoantes);
+    printf("Quantidade de espaços: %d\n", espacos);
+    printf("Quantidade de palavras: %d\n", palavras);
+    
+    return 0;
+}
--- C-projects/lista_strings_v2/ex03.c
+++ C-projects/lista_strings_v2/ex03.c
@@ -1,1 +1,34 @@
 // Ex03) Leia 5 nomes de até 50 caracteres  e armazene em uma matriz de strings. Depois, leia um nome ou parte dele, e pesquisar na matriz e exibir quando encontrado
+
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_NAMES 5
+#define MAX_NAME_LENGTH 50
+
+int main() {
+   
+    char names[NUM_NAMES][MAX_NAME_LENGTH];
+    char searchName[MAX_NAME_LENGTH];
+    
+    // lendo os nomes e armazenando na matriz
+    printf("Digite os %d nomes de até %d caracteres:\n", NUM_NAMES, MAX_NAME_LENGTH);
+    for (int i = 0; i < NUM_NAMES; i++) {
+        printf("Nome %d: ", i + 1);
+        scanf("%s", names[i]);
+    }
+    
+    // pedindo nome/parte dele para ser pesquisado
+    printf("Digite um nome ou parte dele para pesquisar: ");
+    scanf("%s", searchName);
+    
+    
+    printf("Nomes encontrados:\n");
+    for (int i = 0; i < NUM_NAMES; i++) {
+        if (strstr(names[i], searchName) != NULL) {
+            printf("%s\n", names[i]);
+        }
+    }
+    
+    return 0;
+}
