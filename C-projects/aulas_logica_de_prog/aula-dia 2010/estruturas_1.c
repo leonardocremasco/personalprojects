e589dd373b8565245c439b1fec766db23e338bd2
Leonardo Cremasco <leoboscariol@gmail.com>
2023-10-21 17:36:17 -0300

    finishing some activities

--- C-projects/aulas_logica_de_prog/aula-dia 2010/estruturas_1.c
+++ C-projects/aulas_logica_de_prog/aula-dia 2010/estruturas_1.c
@@ -1,44 +1,58 @@
 
 #include <stdio.h>
+
 //variavel global
 int id = 0;
+
 struct data{
     int dia;
     int mes;
     int ano;
 };
-
 struct cadastro{
    char nome[50];
    float peso;
    struct data datanascimento;
 };
 
-
 void incluir(struct cadastro p[10]){
    fflush(stdin);
+
    printf("\nDigite seu nome:"); fgets(p[id].nome,50,stdin);
    printf("\nDigite seu peso:"); scanf("%f",&p[id].peso);
    printf("\nDigite o dia do seu nascimento: ");scanf("%d",&p[id].datanascimento.dia);
    printf("\nDigite o mes do seu nascimento: ");scanf("%d",&p[id].datanascimento.mes);
    printf("\nDigite o ano do seu nascimento: ");scanf("%d",&p[id].datanascimento.ano);
    printf("\nCadastro realizado");
+
    id++;
 };
 void exibir(struct cadastro p[10]){
     int id;
     printf("\nDigite o id da pessoa:"); scanf("%d",&id);
-   printf("\Nome: %s ",p[id].nome);
-   printf("\Peso: %0.2f", p[id].peso);
+   printf("\nNome: %s ",p[id].nome);
+   printf("\nPeso: %0.2f", p[id].peso);
    printf("\nData nascimento: %d/%d/%d",p[id].datanascimento.dia, p[id].datanascimento.mes, p[id].datanascimento.ano);
    system("pause");
 };
+void alterar(struct cadastro p[10]){
+   
+   int id;
+   fflush(stdin);
 
-void alterar(){};
+   printf("\nDigite o id da pessoa: "); scanf("%d", &id);
+   fflush(stdin);
+   printf("\nAltere seu nome:"); fgets(p[id].nome,50,stdin);
+   printf("\nAltere seu peso:"); scanf("%f",&p[id].peso);
+   printf("\nAltere o dia do seu nascimento: ");scanf("%d",&p[id].datanascimento.dia);
+   printf("\nAltere o mes do seu nascimento: ");scanf("%d",&p[id].datanascimento.mes);
+   printf("\nAlteree o ano do seu nascimento: ");scanf("%d",&p[id].datanascimento.ano);
+   printf("\nAlteração realizada");
+   system("pause");
+
+   id++; 
+};
 void excluir(){};
-
-
-
 
 int main(){
 
@@ -57,7 +71,7 @@ int main(){
         incluir(p);
         break;
     case 2:
-        alterar();
+        alterar(p);
         break;
     case 3:
         excluir();
