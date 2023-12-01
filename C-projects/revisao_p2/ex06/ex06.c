fce01b6971914022e93846d767e78e5d71458899
Leonardo Cremasco <leoboscariol@gmail.com>
2023-11-30 21:11:30 -0300

    P2 Review Exercises | finished

--- C-projects/revisao_p2/ex06/ex06.c
+++ C-projects/revisao_p2/ex06/ex06.c
@@ -3,76 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// struct Produto
+// Definindo a struct Produto
 struct Produto {
     int codigo;
     char nome[50];
     float preco;
 };
 
-// cadastrar um novo produto
-void cadastrarProduto() {
-    struct Produto novoProduto;
-
-    printf("Digite o codigo do produto: ");
-    scanf("%d", &novoProduto.codigo);
-
-    printf("Digite o nome do produto: ");
-    scanf("%s", novoProduto.nome);
-
-    printf("Digite o preco do produto: ");
-    scanf("%f", &novoProduto.preco);
-
-    //arquivo para escrita (modo "a" para adicionar ao final do arquivo)
-    FILE *arquivo = fopen("produtos.txt", "a");
-
-    // Verificar se o arquivo foi aberto com sucesso
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        exit(1);
-    }
-
-    // Escrever as informações do produto no arquivo
-    fprintf(arquivo, "%d %s %.2f\n", novoProduto.codigo, novoProduto.nome, novoProduto.preco);
-
-    // Fechar o arquivo
-    fclose(arquivo);
-
-    printf("Produto cadastrado com sucesso!\n");
-}
-
-// Função para exibir todos os produtos cadastrados
-void exibirProdutos() {
-    // Abrir o arquivo para leitura
-    FILE *arquivo = fopen("produtos.txt", "r");
-
-    // Verificar se o arquivo foi aberto com sucesso
-    if (arquivo == NULL) {
-        printf("Nenhum produto cadastrado ainda.\n");
-        return;
-    }
-
-    struct Produto produtoLido;
-
-    // Ler e exibir cada produto do arquivo
-    while (fscanf(arquivo, "%d %s %f", &produtoLido.codigo, produtoLido.nome, &produtoLido.preco) != EOF) {
-        printf("Código: %d, Nome: %s, Preço: %.2f\n", produtoLido.codigo, produtoLido.nome, produtoLido.preco);
-    }
-
-    // Fechar o arquivo
-    fclose(arquivo);
-}
+// Protótipos das funções
+void cadastrarProduto();
+void exibirProdutos();
 
 int main() {
     int escolha;
 
     do {
-        // Menu de opções
+        // Menu principal
         printf("\nMenu:\n");
         printf("1. Cadastrar novo produto\n");
         printf("2. Exibir todos os produtos cadastrados\n");
         printf("3. Sair\n");
-        printf("Escolha uma opcao: ");
+        printf("Escolha a opcao: ");
         scanf("%d", &escolha);
 
         switch (escolha) {
@@ -83,7 +34,7 @@ int main() {
                 exibirProdutos();
                 break;
             case 3:
-                printf("Saindo do programa.\n");
+                printf("Saindo do programa. Adeus!\n");
                 break;
             default:
                 printf("Opcao invalida. Tente novamente.\n");
@@ -93,3 +44,53 @@ int main() {
 
     return 0;
 }
+
+void cadastrarProduto() {
+    struct Produto produto;
+
+    // Solicitando informações do usuário
+    printf("\nDigite o codigo do produto: ");
+    scanf("%d", &produto.codigo);
+    printf("Digite o nome do produto: ");
+    scanf("%s", produto.nome);
+    printf("Digite o preco do produto: ");
+    scanf("%f", &produto.preco);
+
+    // Abrindo o arquivo para escrita
+    FILE *arquivo = fopen("produtos.txt", "a");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Escrevendo as informações no arquivo
+    fprintf(arquivo, "%d %s %.2f\n", produto.codigo, produto.nome, produto.preco);
+
+    // Fechando o arquivo
+    fclose(arquivo);
+
+    printf("Produto cadastrado com sucesso!\n");
+}
+
+void exibirProdutos() {
+    struct Produto produto;
+
+    // Abrindo o arquivo para leitura
+    FILE *arquivo = fopen("produtos.txt", "r");
+
+    if (arquivo == NULL) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    // Lendo e exibindo as informações dos produtos
+    printf("\nProdutos cadastrados:\n");
+    while (fscanf(arquivo, "%d %s %f", &produto.codigo, produto.nome, &produto.preco) != EOF) {
+        printf("Codigo: %d, Nome: %s, Preco: %.2f\n", produto.codigo, produto.nome, produto.preco);
+    }
+
+    // Fechando o arquivo
+    fclose(arquivo);
+}
+
