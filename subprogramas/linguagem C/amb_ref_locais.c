cef89403d95351437d0ab06779bc9592a7d2c86c
Leonardo Cremasco <leoboscariol@gmail.com>
2024-05-29 17:26:57 -0300

    asdasd

--- subprogramas/linguagem C/amb_ref_locais.c
+++ subprogramas/linguagem C/amb_ref_locais.c
@@ -1,6 +1,13 @@
 
+// Ambientes em referenciais locais: variáveis estáticas e dinâmicas
+// Em C, temos ambos os exemplos, onde normalmente seu padrão é a chamada stack-dinâmica, onde por padrão ela  é estática, mas por meio de subprogramas (funções) ou de sentenças propostas pelo código pode ser alterada (tornando-se dinâmica).
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int somador (int array[], int array_tam)
+{
+    static int soma = 0; // --> aqui é onde a variável se torna ESTÁTICA
+    
+}
     
